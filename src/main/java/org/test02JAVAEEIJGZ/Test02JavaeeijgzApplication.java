47b604f0e024f1a39b6053a55f25e5e35a3f3e64
IGabriel30 <ingridgabriel30@hotmail.com>
2024-08-08 09:50:15 -0600

    se inicializa y se eestructura el proyecto

---
+++ src/main/java/org/test02JAVAEEIJGZ/Test02JavaeeijgzApplication.java
@@ -0,0 +1,13 @@
+package org.test02JAVAEEIJGZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeeijgzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeeijgzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/test02JAVAEEIJGZ/Test02JavaeeijgzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test02JAVAEEIJGZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test02JavaeeijgzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
